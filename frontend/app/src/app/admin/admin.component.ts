60364d29abe55f0dc624ca3a742e33c12cca110b
Blodhead <81102425+Blodhead@users.noreply.github.com>
2022-12-30 00:21:30 +0100

    admin reload on same page

--- frontend/app/src/app/admin/admin.component.ts
+++ frontend/app/src/app/admin/admin.component.ts
@@ -11,37 +11,44 @@ import { UserService } from '../user.service';
 export class AdminComponent implements OnInit {
 
 
-  constructor(private service: UserService){}
+  constructor(private service: UserService) { }
 
-active_tab:string = "Organizers";
-allUsers: User[] = [];
-organisers: User[] = [];
-participants: User[] = [];
-requests: User[] = [];
+  active_tab: string = "Organizers";
+  allUsers: User[] = [];
+  organisers: User[] = [];
+  participants: User[] = [];
+  requests: User[] = [];
 
   ngOnInit(): void {
-    this.service.getTempData().subscribe((data: User[])=>{
-      
-      for(var i = 0; i < data.length ; i++){
+    this.service.getTempData().subscribe((data: User[]) => {
 
-        if(data[i].type != "admin")
-        this.allUsers.push(data[i]);
-        
-        if(data[i].status == "waiting")
+      if (localStorage.getItem("type") != null) {
+        const str = localStorage.getItem("type");
+        const str2 = str.charAt(0).toUpperCase() + str.slice(1) +"s";
+        this.active_tab = str2;
+        localStorage.clear();
+      }
+
+      for (var i = 0; i < data.length; i++) {
+
+        if (data[i].type != "admin")
+          this.allUsers.push(data[i]);
+
+        if (data[i].status == "waiting")
           this.requests.push(data[i]);
-          else if(data[i].type == "participant") this.participants.push(data[i]);
-          else if(data[i].type == "organizer") this.organisers.push(data[i]);
+        else if (data[i].type == "participant") this.participants.push(data[i]);
+        else if (data[i].type == "organizer") this.organisers.push(data[i]);
 
       }
     })
   }
 
-  accept(username, password){
-    
-    var user:User;
+  accept(username, password) {
 
-    for(var i = 0; i < this.allUsers.length; i++){
-      if(this.allUsers[i].username == username){
+    var user: User;
+
+    for (var i = 0; i < this.allUsers.length; i++) {
+      if (this.allUsers[i].username == username) {
         user = this.allUsers[i];
         break;
       }
@@ -49,17 +56,17 @@ requests: User[] = [];
 
     user.status = "approved";
 
-    if(!user) alert("Something is wery wrong");
+    if (!user) alert("Something is wery wrong");
 
     this.service.updateStatus(user).subscribe();
     location.reload();
   }
 
-  reject(username, password){
-    var user:User;
+  reject(username, password) {
+    var user: User;
 
-    for(var i = 0; i < this.allUsers.length; i++){
-      if(this.allUsers[i].username == username){
+    for (var i = 0; i < this.allUsers.length; i++) {
+      if (this.allUsers[i].username == username) {
         user = this.allUsers[i];
         break;
       }
@@ -67,29 +74,30 @@ requests: User[] = [];
 
     user.status = "rejected";
 
-    if(!user) alert("Something is wery wrong");
+    if (!user) alert("Something is wery wrong");
 
     this.service.updateStatus(user).subscribe();
     location.reload();
   }
 
-  getTab():string{
+  getTab(): string {
     return this.active_tab;
   }
 
-  setTab(input){
+  setTab(input) {
     this.active_tab = input;
   }
 
 
-  delete(username){
+  delete(username, type) {
     let user = new User();
+    localStorage.setItem("type", type);
     user.username = username.toString();
-    this.service.deleteUser(user).subscribe((user)=>{
+    this.service.deleteUser(user).subscribe((user) => {
       user = null;
       location.reload();
     });
-    
+
 
   }
 }
