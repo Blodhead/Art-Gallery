a1798ccc03c35264aed8038c9b2fa94a537b4f1d
Blodhead <cirkovic32.mi@gmail.com>
2023-01-24 01:26:00 +0100

    chat module

--- frontend/app/src/app/message/message.component.ts
+++ frontend/app/src/app/message/message.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { User } from '../models/user';
 import { Comment } from '../models/workshop-details';
 
 @Component({
@@ -10,16 +11,20 @@ export class MessageComponent implements OnInit {
 
   @Input() myMessage: Comment;
 
-  username: string ="";
-  image: string ="";
-  message: string ="";
+  username: string = "";
+  image: string = "";
+  message: string = "";
   date: Date = null;
-  align: string ="";
+  align: string = "";
+  current_user: User = null;
 
   ngOnInit(): void {
     this.username = this.myMessage.username;
     this.image = this.myMessage.image;
     this.message = this.myMessage.message;
     this.date = this.myMessage.date;
+
+    this.current_user = JSON.parse(localStorage.getItem("current_user"));
+    
   }
 }
