f6e11bad6e3b214861f68203481b15bddbacd5ff
Blodhead <cirkovic32.mi@gmail.com>
2025-10-18 20:13:02 +0200

    Additional style

--- frontend/app/src/app/app.component.ts
+++ frontend/app/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'app';
+  showAuthBg = false;
+  private sub: Subscription;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.sub = this.router.events.subscribe((evt) => {
+      if (evt instanceof NavigationEnd) {
+        const url = evt.urlAfterRedirects || evt.url;
+        this.showAuthBg = url.startsWith('/login') || url.startsWith('/register');
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.sub) this.sub.unsubscribe();
+  }
+
 }
+
