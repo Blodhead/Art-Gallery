3afab9bad732facc71a560a0ee873ae775461f6c
Blodhead <cirkovic32.mi@gmail.com>
2025-10-26 08:33:21 +0100

    More parfume images

--- frontend/app/src/environments/environment.ts
+++ frontend/app/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  //apiUrl: "http://localhost:4000", // default to same origin; set in production build if needed
-  apiUrl: 'https://finestservices.onrender.com' 
+  apiUrl: "http://localhost:4000", // default to same origin; set in production build if needed
+  //apiUrl: 'https://finestservices.onrender.com' 
 };
