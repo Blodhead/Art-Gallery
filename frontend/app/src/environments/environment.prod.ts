1da3698913112dd74847b42f72f2b07ca891ee93
Blodhead <cirkovic32.mi@gmail.com>
2025-10-29 19:55:28 +0100

    env config

--- frontend/app/src/environments/environment.prod.ts
+++ frontend/app/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://finestservices.onrender.com' // replace with your production API host or leave empty to use same origin
+  apiUrl: "https://finestservices.onrender.com"
 };
